284a064f8e6e1271c372398103eddce2b8e3caad
wangxianqiao <wangxianqiao@cloudfine.com>
2022-06-30 01:58:32 +0800

    fullscreen bug

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .menu(tauri::Menu::os_default(&context.package_info().name))
+    .on_window_event(|event| match event.event() {
+        tauri::WindowEvent::Resized(_) => {
+            println!("is_fullscreen {:?}", event.window().is_fullscreen());
+        }
+        _ => {}
+    })
     .run(context)
     .expect("error while running tauri application");
 }
